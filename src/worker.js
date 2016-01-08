14a93185015f6ce12c4abd53483e86e399254e68
Shafique Arnab <shafique@buncee.com>
2016-01-07 20:00:59 -0500

    Version bump

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,6 @@
 define(function () {
 	return {
-		version: "1.0.1",
+		version: "2.0",
 		load: function (name, req, onLoad, config) {
 			if (config.isBuild) {
 				//don't do anything if this is a build, can't inline a web worker
